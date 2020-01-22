da7298f6ee9419fb053e0b386592dc719af7b332
Eric Normand <ericwnormand@gmail.com>
2020-01-22 10:39:37 -0600

    Basic markdown editor

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -1,8 +1,50 @@
-(ns markdownify.main)
+(ns markdownify.main
+  (:require [reagent.core :as reagent]
+            ["showdown" :as showdown]))
+
+(defonce markdown (reagent/atom ""))
+
+(defonce showdown-converter (showdown/Converter.))
+
+(defn md->html [md]
+  (.makeHtml showdown-converter md))
+
+(defn app []
+  [:div
+   [:h1 "Markdownify"]
+   [:div
+    {:style {:display :flex}}
+    [:div
+     {:style {:flex "1"}}
+     [:h2 "Markdown"]
+     [:textarea
+      {:on-change #(reset! markdown (-> % .-target .-value))
+       :value @markdown
+       :style {:resize "none"
+               :height "500px"
+               :width "100%"}}]]
+
+    [:div
+     {:style {:flex "1"
+              :padding-left "2em"}}
+     [:h2 "HTML Preview"]
+     [:div {:style {:height "500px"}
+            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]]) 
+
+
+
+
+
+
+
+
+(defn mount! []
+  (reagent/render [app]
+                  (.getElementById js/document "app")))
 
 (defn main! []
-  (println "Welcome to the app!"))
+  (mount!))
 
 (defn reload! []
-  (println "Reloaded!"))
+  (mount!))
 
